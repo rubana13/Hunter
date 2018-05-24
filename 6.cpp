16d4c1757e05f8d643829d7109e3c45e25a826e3
rubana13 <36365491+rubana13@users.noreply.github.com>
2018-05-24 15:52:07 +0530

    to find the first repeating number;

---
+++ 6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+  int arr[100],i,j,count=0,n;
+  cin>>n;
+  if((n>=1)&&(n<=10000))
+  {
+  for(i=0;i<n;i++)
+  {
+      cin>>arr[i];
+  }
+  }
+  else
+  {
+      cout<<"Invalid output";
+  }
+  for(i=0;i<n;i++)
+  {
+      for(j=0;j<n;j++)
+      {
+            if (i != j && arr[i] == arr[j])
+               count++;
+      }
+  }         
+        
+    if(count==1)
+    {
+        cout<<arr[i];
+       
+    }
+    else
+    {
+        cout<<"Unique";
+    }
+
+  
+
+}
